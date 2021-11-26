714ae289e10bc7475d5129cc6070d1b8075cc8c7
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2021-11-26 16:51:42 +0300

    FIR: Fix synthetic type variables to independent stub types

--- compiler/fir/cones/src/org/jetbrains/kotlin/fir/types/ConeTypes.kt
+++ compiler/fir/cones/src/org/jetbrains/kotlin/fir/types/ConeTypes.kt
@@ -314,7 +314,11 @@ fun ConeIntersectionType.mapTypes(func: (ConeKotlinType) -> ConeKotlinType): Con
     return ConeIntersectionType(intersectedTypes.map(func), alternativeType?.let(func))
 }
 
-data class ConeStubTypeConstructor(val variable: ConeTypeVariable, val isTypeVariableInSubtyping: Boolean) : TypeConstructorMarker
+data class ConeStubTypeConstructor(
+    val variable: ConeTypeVariable,
+    val isTypeVariableInSubtyping: Boolean,
+    val isForFixation: Boolean = false,
+) : TypeConstructorMarker
 
 sealed class ConeStubType(val constructor: ConeStubTypeConstructor, override val nullability: ConeNullability) : StubTypeMarker,
     ConeSimpleKotlinType() {
@@ -354,6 +358,9 @@ open class ConeStubTypeForBuilderInference(constructor: ConeStubTypeConstructor,
         ), nullability
     )
 }
+
+class ConeStubTypeForFixation(constructor: ConeStubTypeConstructor, nullability: ConeNullability) :
+    ConeStubTypeForBuilderInference(constructor, nullability)
 
 class ConeStubTypeForTypeVariableInSubtyping(constructor: ConeStubTypeConstructor, nullability: ConeNullability) :
     ConeStubType(constructor, nullability) {
